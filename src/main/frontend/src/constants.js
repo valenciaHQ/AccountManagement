61fab938f856e8db165ecbb26416b7786590e338
Alejandro Valencia <alejandro.valencia@valtech.com>
2019-05-30 18:12:07 -0300

    AAW-3557: minor improvements

--- src/main/frontend/src/constants.js
+++ src/main/frontend/src/constants.js
@@ -2,6 +2,6 @@ export default {
     ERROR_FETCH : 'An error occurred trying fetch account data',
     ERROR_DEBIT_SUBMIT : 'An error occurred trying to perform debit transaction',
     ERROR_CREDIT_SUBMIT : 'An error occurred trying to perform credit transaction',
-    ERROR_EMPTY_OR_ZERO_CREDIT : "Credit must not be empty or zero",
-    ERROR_EMPTY_OR_ZERO_DEBIT : "Debit must not be empty or zero",
+    ERROR_EMPTY_OR_ZERO_CREDIT : "Credit must not be empty or less than zero",
+    ERROR_EMPTY_OR_ZERO_DEBIT : "Debit must not be empty or less than zero",
 }
